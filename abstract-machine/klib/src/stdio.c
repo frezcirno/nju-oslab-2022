efb2c4e7f892d261863fd75c59f4652ca3559336
frezcirno <2277861660@qq.com>
2022-06-09 23:31:54 +0800

    add klib

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -6,24 +6,139 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+char *convert(char *buf, unsigned int num, int base)
+{
+  const char *digits = "0123456789ABCDEF";
+  char *ptr;
+
+  ptr = &buf[49];
+  *ptr = '\0';
+
+  do {
+      *--ptr = digits[num % base];
+      num /= base;
+  } while (num);
+
+  return ptr;
+}
+
+#define PARSE(fmt, ap, collect)      \
+  unsigned int __i;                  \
+  char __buf[50];                    \
+  char *__s;                         \
+                                     \
+  while (*fmt)                       \
+  {                                  \
+    while (*fmt && *fmt != '%')      \
+    {                                \
+      collect(*fmt);                 \
+      fmt++;                         \
+    }                                \
+    if (!*fmt)                       \
+      break;                         \
+    fmt++;                           \
+    switch (*fmt)                    \
+    {                                \
+    case 'c':                        \
+      __i = va_arg(ap, int);         \
+      collect(__i);                  \
+      break;                         \
+                                     \
+    case 'd':                        \
+      __i = va_arg(ap, int);         \
+      if (__i < 0)                   \
+      {                              \
+        __i = -__i;                  \
+        collect('-');                \
+      }                              \
+      __s = convert(__buf, __i, 10); \
+      putstr(__s);                   \
+      break;                         \
+                                     \
+    case 'o':                        \
+      __i = va_arg(ap, int);         \
+      if (__i < 0)                   \
+      {                              \
+        __i = -__i;                  \
+        collect('-');                \
+      }                              \
+      __s = convert(__buf, __i, 8);  \
+      putstr(__s);                   \
+      break;                         \
+                                     \
+    case 'x':                        \
+      __i = va_arg(ap, int);         \
+      if (__i < 0)                   \
+      {                              \
+        __i = -__i;                  \
+        collect('-');                \
+      }                              \
+      __s = convert(__buf, __i, 16); \
+      putstr(__s);                   \
+      break;                         \
+                                     \
+    case 's':                        \
+      __s = va_arg(ap, char *);      \
+      putstr(__s);                   \
+      break;                         \
+                                     \
+    default:                         \
+      break;                         \
+    }                                \
+    fmt++;                           \
+  }
+
+int vfprintf(const char *fmt, va_list ap)
+{
+#define collect(char) putch(char)
+  PARSE(fmt, ap, collect);
+#undef collect
+  return 0;
+}
+
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+  int res;
+  va_list ap;
+  va_start(ap, fmt);
+  res = vfprintf(fmt, ap);
+  va_end(ap);
+  return res;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  int i = 0;
+#define collect(char) ({ i++; *out++ = (char); })
+  PARSE(fmt, ap, collect);
+#undef collect
+  *out = '\0';
+  return i;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  int res;
+  va_list ap;
+  va_start(ap, fmt);
+  res = vsprintf(out, fmt, ap);
+  va_end(ap);
+  return res;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
+  int res;
+  va_list ap;
+  va_start(ap, fmt);
+  res = vsnprintf(out, n, fmt, ap);
+  va_end(ap);
+  return res;
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  size_t i = 0;
+#define collect(char) ({ if (i<n) { i++; *out++ = (char); } })
+  PARSE(fmt, ap, collect);
+#undef collect
+  *out = '\0';
+  return 0;
 }
 
 #endif
--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,43 +5,74 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  size_t len = 0;
+  while (*s++)
+    len++;
+  return len;
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  for (; *src; dst++, src++)
+    *dst = *src;
+  return dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  for (size_t i = 0; *src && i < n; i++, dst++, src++)
+    *dst = *src;
+  return dst;
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  while (*dst)
+    dst++;
+  for (; *src; dst++, src++)
+    *dst = *src;
+  return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  for (; *s1 && *s2; s1++, s2++)
+    if (*s1 != *s2)
+      return *s1 - *s2;
+  return *s1 - *s2;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+  for (size_t i = 0; *s1 && *s2 && i < n; i++, s1++, s2++)
+    if (*s1 != *s2)
+      return *s1 - *s2;
+  return *s1 - *s2;
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  for (size_t i = 0; i < n; i++)
+    ((char *)s)[i] = c;
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
+  if (src < dst && (char *)dst < (char *)src + n)
+  {
+    for (size_t i = n - 1; i >= 0; i--)
+      i[(char *)dst] = i[(char *)src];
+  }
+  else
+    memcpy(dst, src, n);
+  return dst;
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+  for (size_t i = 0; i < n; i++)
+    i[(char *)out] = i[(char *)in];
+  return out;
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  for (size_t i = 0; i < n; i++)
+    if (*(char *)s1 != *(char *)s2)
+      return *(char *)s1 - *(char *)s2;
+  return *(char *)s1 - *(char *)s2;
 }
 
 #endif
